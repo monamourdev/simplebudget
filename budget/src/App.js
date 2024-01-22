1c4a431dc998852726e94d762181e44bea7c3d2a
monamour.dev <tiagomendonca.dev@gmail.com>
2024-01-21 23:47:04 -0300

    Feat: pass event by props

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-// import Evento from './components/Evento'
+import Evento from './components/Evento'
 import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <h1>Testando Eventos</h1>
-
+      <Evento numero="1"/>
       <Form />
     </div>
 
--- budget/src/components/Evento.js
+++ budget/src/components/Evento.js
@@ -1,17 +1,24 @@
-// function Evento({numero}){
+import Button from "./evento/Button";
 
-//     function meuEvento(){
-//         console.log("Loguei:" + numero)
-//     }
+function Evento(){
 
-//     return (
-//         <div>
-//             <p>Clique para disparar um evento:</p>
-//             <button onClick={meuEvento}>Ativar!</button>
-//         </div>
+    function meuEvento(){
+        console.log("Ativando primeiro evento")
+    }
+
+    function segundoEvento(){
+        console.log("Ativando segundo evento")
+    }
+
+    return (
+        <div>
+            <p>Clique para disparar um evento:</p>
+            <Button event={meuEvento} text="Primeiro Evento"/>
+            <Button event={segundoEvento} text="Segundo Evento"/>
+        </div>
 
 
-//     )
-// }
+    )
+}
 
-// export default Evento;
+export default Evento;
