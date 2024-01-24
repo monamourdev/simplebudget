518098d6460ab566cb616a7d76013ec109dd6b67
monamour.dev <tiagomendonca.dev@gmail.com>
2024-01-24 01:50:54 +0100

    Feat: React Router Implements

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,17 +1,26 @@
-import './App.css';
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
-import { useState } from 'react';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './pages/Home'
+import Empresa from './pages/Empresa'
+import Contato from './pages/Contato'
+import React from 'react'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
-  const [nome, setNome] = useState()
 
   return (
-    <div className="App">
-      <h1>State Lift</h1>
-      <SeuNome setNome={setNome}/>
-      <Saudacao nome={nome} />
-    </div>
+
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/empresa" element={<Empresa />}/>
+        <Route path="/contato" element={<Contato />}/>
+      </Routes>
+      
+      <Footer />
+    </Router>
 
   );
 }
---
+++ budget/src/pages/Empresa.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Empresa(){
+    return(
+        <div>
+            <h1>Empresa</h1>
+            <p>Conteúdo da Página</p>
+        </div>
+    )
+}
+
+export default Empresa;
