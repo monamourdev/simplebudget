28b1159b4055e1885cdc8d3fd6a78b076f294f7f
monamour.dev <tiagomendonca.dev@gmail.com>
2024-01-25 22:44:49 -0300

    feat: project config

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,26 +1,10 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
-import Home from './pages/Home'
-import Empresa from './pages/Empresa'
-import Contato from './pages/Contato'
 import React from 'react'
-import Navbar from './components/layout/Navbar'
-import Footer from './components/layout/Footer'
 
 function App() {
 
   return (
 
-    <Router>
-      <Navbar />
-
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/empresa" element={<Empresa />}/>
-        <Route path="/contato" element={<Contato />}/>
-      </Routes>
-      
-      <Footer />
-    </Router>
+    <p>Costs</p>
 
   );
 }
