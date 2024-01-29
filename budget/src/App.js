1855b4dedfb1bbc284f0cf6e1f13ecb3db9013fc
monamour.dev <tiagomendonca.dev@gmail.com>
2024-01-29 14:01:58 -0300

    feat: Structure project

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,10 +1,35 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import React from 'react'
+
+import NewProject from './components/pages/NewProject'
+import Company from './components/pages/Company'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Container from './components/layout/Container'
 
 function App() {
 
   return (
 
-    <p>Costs</p>
+
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">contact</Link>
+        <Link to="/Company">Company</Link>
+        <Link to="/newproject">NewProject</Link>
+
+      </div>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
 
   );
 }
