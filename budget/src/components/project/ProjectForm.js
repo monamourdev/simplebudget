15de8ad07d83df33828618dfd69bad93c35fc4cf
monamour.dev <tiagomendonca.dev@gmail.com>
2024-01-30 19:04:16 -0300

    feat: Conect categories with json-server backend

--- budget/src/components/project/ProjectForm.js
+++ budget/src/components/project/ProjectForm.js
@@ -1,9 +1,29 @@
+import { useState, useEffect } from 'react'
+
 import styles from './ProjectForm.module.css'
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 
 function ProjectForm() {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'content-Type': 'application/json'
+            }
+        }).then(
+            resp => resp.json()
+        )
+            .then(data => setCategories(data))
+            .catch(
+                err => console.log(err)
+            )
+    }, [])
+
     return (
         <form className={styles.form}>
             <Input
@@ -13,17 +33,18 @@ function ProjectForm() {
                 placeholder="Insira o nome do projeto"
             />
             <div>
-            <Input
-                type="number"
-                text="Orçamento do projeto"
-                name="budget"
-                placeholder="Insira o orçamento total"
-            />
+                <Input
+                    type="number"
+                    text="Orçamento do projeto"
+                    name="budget"
+                    placeholder="Insira o orçamento total"
+                />
             </div>
             <div>
-                <Select 
+                <Select
                     name="category_id"
-                    text="Selecione a categoria" 
+                    text="Selecione a categoria"
+                    options={categories}
                 />
 
 
