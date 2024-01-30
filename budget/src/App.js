603156711c193cf7d2095fd2ae98ff93df066008
monamour.dev <tiagomendonca.dev@gmail.com>
2024-01-29 21:20:09 -0300

    feat: Create navbar and footer content

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,37 +1,35 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import React from 'react'
 
 import NewProject from './components/pages/NewProject'
 import Company from './components/pages/Company'
 import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
+import Projects from './components/pages/Projects'
+
 import Container from './components/layout/Container'
+
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
 
   return (
-
-
-    <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">contact</Link>
-        <Link to="/Company">Company</Link>
-        <Link to="/newproject">NewProject</Link>
-
-      </div>
-      <Container customClass="min-height">
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/company" element={<Company />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/newproject" element={<NewProject />} />
-        </Routes>
-      </Container>
-      <p>Footer</p>
-    </Router>
-
+      <Router>
+        <Navbar />
+        <Container customClass="min-height">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/company" element={<Company />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/newproject" element={<NewProject />} />
+          </Routes>
+        </Container>
+        <Footer />
+      </Router>
   );
 }
 
 export default App;
+ 
