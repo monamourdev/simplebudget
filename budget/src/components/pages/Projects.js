7aa07916d5c32161053944645e8e9bffb32cbb9b
monamour.dev <tiagomendonca.dev@gmail.com>
2024-01-30 22:03:12 -0300

    feat: create dashboard projects

--- budget/src/components/pages/Projects.js
+++ budget/src/components/pages/Projects.js
@@ -1,6 +1,10 @@
 import Message from "../layout/Message";
 import {useLocation} from 'react-router-dom'
 
+import Container from '../layout/Container'
+import styles from "./Projects.module.css"
+import LinkButton from '../layout/LinkButton'
+
 function Projects(){
 
     const location = useLocation();
@@ -11,11 +15,17 @@ function Projects(){
     }
 
     return(
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />
+                
+            </div>
             {message && 
-            <Message msg={message} type="success"/>
-            }
+            <Message msg={message} type="success"/>}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
